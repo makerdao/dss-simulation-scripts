49c47b1bc35b1980f8efc663ae8a641d425d579c
Tal Baneth <talbaneth@gmail.com>
2021-11-22 11:29:04 +0200

    Fix storage slot read in bin/osm_price.sh

--- bin/osm_price.sh
+++ bin/osm_price.sh
@@ -3,7 +3,7 @@
 export ETH_RPC_URL="http://127.0.0.1:8545/"
 export OSM=${1}
 
-rawStorage=$(seth storage "${OSM}" 4)
+rawStorage=$(seth storage "${OSM}" 3)
 currentPrice=$(seth --from-wei "$(seth --to-dec "${rawStorage:34:32}")")
 
 echo "${currentPrice}"
