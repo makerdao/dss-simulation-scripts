85028af2c91e7a8c4a505522d8ca4772e7f822dc
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-04-05 17:01:49 -0400

    feat: use cast to cage MCD

--- contracts/Action.sol
+++ contracts/Action.sol
@@ -3,11 +3,21 @@
 pragma solidity ^0.6.12;
 
 interface PipLike { 
-  function change(address src) external;
+    function change(address src) external;
+}
+
+interface Cageable {
+    function cage() external;
 }
 
 contract Action {
-  function change(address pip, address src) external {
-    PipLike(pip).change(src);
-  }
+
+    function change(address pip, address src) external {
+        PipLike(pip).change(src);
+    }
+
+    function cage(address end) external {
+        Cageable(end).cage();
+    }
+
 }
--- scripts/cageMCD.js
+++ scripts/cageMCD.js
@@ -1,60 +1,21 @@
 #!/usr/bin/env node
 
+const assert = require("assert");
 const fs = require('fs');
 const hre = require("hardhat");
 const ethers = hre.ethers;
+const cast = require("./cast.js");
 
-const args = process.argv.slice(2);
-
-const ETH_FROM    = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-const PAUSE_PROXY = "0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB";
 const MCD_END     = "0xBB856d1742fD182a90239D7AE85706C2FE4e5922";
 
-// test voidOSM
+// test cageMCD
 async function main() {
-  let signer = await ethers.getSigner(ETH_FROM);
-  let signerAddress = await signer.getAddress();
-
-  // balance PAUSE_PROXY
-  balance = await ethers.provider.getBalance(PAUSE_PROXY);
-  console.log(PAUSE_PROXY + ': ' + ethers.utils.formatEther(balance));
-
-  // send eth to the pause proxy
-  await hre.network.provider.send("hardhat_setCoinbase", [PAUSE_PROXY]);
-  await hre.network.provider.send("evm_mine");
-
-  // balance PAUSE_PROXY
-  balance = await ethers.provider.getBalance(PAUSE_PROXY);
-  console.log(PAUSE_PROXY + ': ' + ethers.utils.formatEther(balance));
-
-  await hre.network.provider.request({
-    method: "hardhat_impersonateAccount",
-    params: [PAUSE_PROXY]
-  });
-
-  signer = await ethers.getSigner(PAUSE_PROXY);
-
-  //
-  // Everthing below here happens as the pause proxy
-  //
   const ENDABI = JSON.parse(fs.readFileSync('./abi/end.json').toString());
-  const end = await ethers.getContractAt(ENDABI, MCD_END, signer);
-
-  await hre.network.provider.request({
-    method: "hardhat_impersonateAccount",
-    params: [PAUSE_PROXY]
-  });
+  const end = await ethers.getContractAt(ENDABI, MCD_END);
 
   // This will cage MCD
-  // await end.cage();
-  await end["cage()"]();
-
-  // switch back to ETH_FROM
-  await hre.network.provider.request({
-    method: "hardhat_stopImpersonatingAccount",
-    params: [PAUSE_PROXY]
-  });
-  signer = await ethers.getSigner(ETH_FROM);
+  await cast("cage(address)", [MCD_END]);
+  assert.equal(await end.live(), 0);
 }
 
 main()
