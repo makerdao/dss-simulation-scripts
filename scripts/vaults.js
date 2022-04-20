289afacaf612c5e2bf06cc1a2ac35cf3b5aff49a
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-04-19 22:43:04 -0400

    feat: get undercollateralized vaults

--- scripts/vaults.js
+++ scripts/vaults.js
@@ -1,6 +1,14 @@
+const hre = require("hardhat");
+const ethers = hre.ethers;
+const chainlog = require("./chainlog.js");
+
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const login = async () => {
+  if (!process.env["API_USERNAME"] || !process.env["API_PASSWORD"]) {
+    console.error("API_USERNAME and API_PASSWORD env vars not defined");
+    process.exit();
+  }
   const url = "https://data-api.makerdao.network/v1/login/access-token";
   const params = new URLSearchParams();
   params.append("username", process.env["API_USERNAME"]);
@@ -11,32 +19,54 @@ const login = async () => {
   return token;
 }
 
-const getVaults = async (token, ilk) => {
+const getUrns = async (token, ilk) => {
   const url = "https://data-api.makerdao.network/v1/vaults/current_state";
   const params = new URLSearchParams();
   params.append("ilk", ilk);
   const authHeader = {Authorization: "Bearer " + token};
   params.append("limit", 1000000);
   const urlWithParams = url + "?" + params.toString();
+  console.log("getting vault addresses…");
   const response = await fetch(urlWithParams, {headers: authHeader});
-  const data = await response.json();
-  return data;
+  console.log("done.");
+  const vaults = await response.json();
+  const urns = [];
+  for (vault of vaults) {
+    urns.push(vault.urn);
+  }
+  return urns;
 }
 
-const filter = vaults => {
-  for (const vault of vaults) {
-    const {debt, collateral, osm_price: price, ratio} = vault;
-    const inkSpot = collateral * price / ratio;
-    if (debt > inkSpot) {
-      console.log(vault);
+const getUnder = async (ilk, urns) => {
+  const vatAbi = [
+    "function ilks(bytes32) external view returns (uint256,uint256,uint256,uint256,uint256)",
+    "function urns(bytes32,address) external view returns (uint256,uint256)"
+  ];
+  const vatAddr = await chainlog("MCD_VAT");
+  const vat = await ethers.getContractAt(vatAbi, vatAddr);
+  const ilk32 = ethers.utils.formatBytes32String(ilk);
+  const [, rate, spot] = await vat.ilks(ilk32);
+  let counter = 0;
+  const under = [];
+  console.log("looking for undercollateralized vaults…");
+  for (const urn of urns) {
+    const percentage = Math.round(100 * counter++ / urns.length);
+    process.stdout.write(`${percentage}% : ${under.length} vaults found\r`);
+    const [ink, art] = await vat.urns(ilk32, urn);
+    if (art.mul(rate).gt(ink.mul(spot))) {
+      console.log(urn);
+      under.push(urn);
     }
   }
+  console.log("");
+  console.log(under.length + " vaults found");
+  return under;
 }
 
 const vaults = async () => {
   const token = await login();
-  const vaults = await getVaults(token, "ETH-A");
-  filter(vaults);
+  const urns = await getUrns(token, "ETH-A");
+  const under = await getUnder("ETH-A", urns);
 }
 
 vaults();
