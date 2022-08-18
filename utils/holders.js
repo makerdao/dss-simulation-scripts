03597d72b3d9531fb69eb743eb396d1c6fb3df37
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-08-18 17:36:30 +0200

    feat: promisify holders

--- utils/holders.js
+++ utils/holders.js
@@ -1,24 +1,24 @@
 const snowflake = require("snowflake-sdk");
 
-const blockNumber = 15364430;
+const query = (blockNumber, onLoad, onFail) => {
 
-const connection = snowflake.createConnection({
-  account: process.env["SNOWFLAKE_ACCOUNT"],
-  username: process.env["SNOWFLAKE_USER"],
-  password: process.env["SNOWFLAKE_PASS"],
-  application: "ES-simulations",
-  database: "ETH",
-  schema: "RAW",
-  jsTreatIntegerAsBigInt: true,
-});
-connection.connect((err, conn) => {
-  if (err) {
-    console.error(`error connecting: ${err.message}`);
-  } else {
-    const connId = conn.getId();
-    console.log(`opened connection ${connId}`);
-    const stmt = conn.execute({
-      sqlText: `
+  const connection = snowflake.createConnection({
+    account: process.env["SNOWFLAKE_ACCOUNT"],
+    username: process.env["SNOWFLAKE_USER"],
+    password: process.env["SNOWFLAKE_PASS"],
+    application: "ES-simulations",
+    database: "ETH",
+    schema: "RAW",
+    jsTreatIntegerAsBigInt: true,
+  });
+  connection.connect((err, conn) => {
+    if (err) {
+      onFail(`error connecting: ${err.message}`);
+    } else {
+      const connId = conn.getId();
+      console.log(`opened connection ${connId}`);
+      const stmt = conn.execute({
+        sqlText: `
 select distinct substr(location, 3, 42) as holder,
     tools.public.hextoint(last_value(curr_value) over (partition by location order by block, order_index)) as balance
 from storage_diffs
@@ -26,24 +26,35 @@ where contract = '0x6b175474e89094c44da98b954eedeac495271d0f' and
     location like '2[%].0' and status and block <= :1
 qualify balance > 0
 order by balance desc;
-      `,
-      binds: [blockNumber],
-      complete: (err, stmt, rows) => {
-        if (err) {
-          console.error(`error executing: ${err.message}`);
-        } else {
-          console.log(`executed: ${stmt.getSqlText()}`);
-          console.log(`produced ${rows.length} rows`);
-          console.log(rows.splice(0, 10));
-          conn.destroy((err, conn) => {
-            if (err) {
-              console.error(`error disconnecting: ${err.message}`);
-            } else {
-              console.log(`closed connection ${connId}`);
-            }
-          });
+        `,
+        binds: [blockNumber],
+        complete: (err, stmt, rows) => {
+          if (err) {
+            onFail(`error executing: ${err.message}`);
+          } else {
+            console.log(`obtained ${rows.length} rows`);
+            conn.destroy((err, conn) => {
+              if (err) {
+                onFail(`error disconnecting: ${err.message}`);
+              } else {
+                console.log(`closed connection ${connId}`);
+                onLoad(rows);
+              }
+            });
+          }
         }
-      }
-    });
-  }
-});
+      });
+      console.log("executing query…");
+    }
+  });
+}
+
+const get = blockNumber => {
+  return new Promise((resolve, reject) => {
+    query(blockNumber, resolve, reject);
+  });
+}
+
+module.exports = {
+  get: get,
+}
