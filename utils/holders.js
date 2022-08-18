be901135a95c8619012c07579741a00116eed2ed
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-08-18 16:19:09 +0200

    feat: add holders query

--- utils/holders.js
+++ utils/holders.js
@@ -6,7 +6,8 @@ const connection = snowflake.createConnection({
   password: process.env["SNOWFLAKE_PASS"],
   application: "ES-simulations",
   database: "ETH",
-  schema: "RAW"
+  schema: "RAW",
+  jsTreatIntegerAsBigInt: true,
 });
 connection.connect((err, conn) => {
   if (err) {
@@ -15,12 +16,22 @@ connection.connect((err, conn) => {
     const connId = conn.getId();
     console.log(`opened connection ${connId}`);
     const stmt = conn.execute({
-      sqlText: 'create database testdb',
+      sqlText: `
+select distinct substr(location, 3, 42) as holder,
+    tools.public.hextoint(last_value(curr_value) over (partition by location order by block, order_index)) as balance
+from storage_diffs
+where contract = '0x6b175474e89094c44da98b954eedeac495271d0f' and
+    location like '2[%].0' and status and block <= 15364430
+qualify balance > 0
+order by balance desc;
+      `,
       complete: (err, stmt, rows) => {
         if (err) {
           console.error(`error executing: ${err.message}`);
         } else {
           console.log(`executed: ${stmt.getSqlText()}`);
+          console.log(`produced ${rows.length} rows`);
+          console.log(rows.splice(0, 10));
           conn.destroy((err, conn) => {
             if (err) {
               console.error(`error disconnecting: ${err.message}`);
@@ -31,5 +42,6 @@ connection.connect((err, conn) => {
         }
       }
     });
+    console.log(stmt);
   }
 });
